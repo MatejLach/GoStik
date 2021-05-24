bd181817be94438839495146f06debbd057773f3
Matej Lach <me@matej-lach.me>
2021-05-24 23:51:07 +0100

    radio init

--- lostik/lostik.go
+++ lostik/lostik.go
@@ -2,12 +2,17 @@ package lostik
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 	"strings"
 	"time"
 
 	"go.bug.st/serial"
+)
+
+var (
+	RadioInitErr = errors.New("problem initialising radio")
 )
 
 type LoStik struct {
@@ -33,30 +38,51 @@ func New(devicePortName string, baudRate int) (LoStik, error) {
 }
 
 func (ls LoStik) RadioInit(initCmds ...string) error {
+	// wake the stick up
+	err := ls.writeCmd("sys get ver")
+	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	_, err = ls.readResp(ctx)
+	if err != nil {
+		return err
+	}
+
+	// give it some breathing space before radio init
+	time.Sleep(1 * time.Second)
+
 	if len(initCmds) == 0 {
 		initCmds = []string{
-			"sys get ver",
+			"radio get mod",
+			"radio get sf",
+			"mac pause",
+			"radio set pwr 10",
 		}
 	}
 
-	for _, cmd := range initCmds {
-		if cmd != "" {
-			err := ls.writeCmd(cmd)
-			if err != nil {
-				return err
-			}
-		}
+	err = ls.execCmds(initCmds)
+	if err != nil {
+		return err
 	}
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
 
 	resp, err := ls.readResp(ctx)
 	if err != nil {
 		return err
 	}
 
-	fmt.Println(resp)
+	r := strings.Split(resp, "\n")
+
+	if len(r) != 4 {
+		return RadioInitErr
+	}
+
+	if r[3] != "ok" {
+		return RadioInitErr
+	}
 
 	return nil
 }
@@ -76,7 +102,6 @@ func (ls LoStik) readResp(ctx context.Context) (string, error) {
 	errC := make(chan error)
 
 	go func(done chan<- struct{}, errCh chan<- error) {
-
 		for {
 			bts := make([]byte, 100)
 			n, err := ls.devicePort.Read(bts)
@@ -93,9 +118,9 @@ func (ls LoStik) readResp(ctx context.Context) (string, error) {
 				return
 			}
 
-			_, _ = sb.Write(bts)
+			_, _ = sb.Write(bts[:n])
 
-			if strings.HasPrefix(string(bts), "ok\r") || strings.HasPrefix(string(bts), "invalid_param\r") || strings.HasPrefix(string(bts), "err\r") {
+			if strings.Contains(string(bts), "ok\r") || strings.HasSuffix(string(bts), "\r") {
 				done <- struct{}{}
 				return
 			}
@@ -107,9 +132,22 @@ func (ls LoStik) readResp(ctx context.Context) (string, error) {
 		case <-ctx.Done():
 			return "", ctx.Err()
 		case <-doneC:
-			return strings.Trim(sb.String(), "\n"), nil
+			return strings.TrimSuffix(strings.Replace(sb.String(), "\r", "", -1), "\n"), nil
 		case err := <-errC:
 			return "", err
 		}
 	}
+}
+
+func (ls LoStik) execCmds(cmds []string) error {
+	for _, cmd := range cmds {
+		if cmd != "" {
+			err := ls.writeCmd(cmd)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
